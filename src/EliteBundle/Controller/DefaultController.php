9e4d48d4668fdbbe1ded360af821f3b1480f0181
Aaron -.- <omgbloodypass@gmail.com>
2015-07-23 23:29:22 +0100

    removed php storm fiels

--- src/EliteBundle/Controller/DefaultController.php
+++ src/EliteBundle/Controller/DefaultController.php
@@ -3,17 +3,11 @@
 namespace EliteBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
-use Symfony\Component\HttpFoundation\Response;
 
 class DefaultController extends Controller
 {
     public function indexAction($name)
     {
         return $this->render('EliteBundle:Default:index.html.twig', array('name' => $name));
-    }
-
-    public function testAction()
-    {
-        return new Response("Hello World");
     }
 }
