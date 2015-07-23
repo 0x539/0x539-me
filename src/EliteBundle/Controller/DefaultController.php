7760b0e94159163b8f5cd8d2c0e2e72c5ed80028
Aaron -.- <omgbloodypass@gmail.com>
2015-07-23 23:18:58 +0100

    got bundle working

--- src/EliteBundle/Controller/DefaultController.php
+++ src/EliteBundle/Controller/DefaultController.php
@@ -3,11 +3,17 @@
 namespace EliteBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Response;
 
 class DefaultController extends Controller
 {
     public function indexAction($name)
     {
         return $this->render('EliteBundle:Default:index.html.twig', array('name' => $name));
+    }
+
+    public function testAction()
+    {
+        return new Response("Hello World");
     }
 }
